9576864f69b6e9298a02d687597d796a7f374c08
Vitaly Krenel <vitaly.post@yahoo.com>
2018-08-27 02:24:02 +1100

    Add handlers definitions for menu events

---
+++ assets/js/initMenu.js
@@ -0,0 +1,27 @@
+import Menu from './menu.js';
+
+const toggleNode = document.querySelector('.js-menu-toggle');
+const menuNode = document.querySelector('.js-menu-container');
+
+const menu = new Menu(toggleNode, menuNode, {
+  activeClass: 'app__nav-container--open',
+});
+
+function toggleMenu(e) {
+  menu.toggle();
+  e.stopPropagation();
+  return false;
+}
+
+function hideMenu() {
+  if (menu.isActive()) {
+    menu.hide();
+  }
+
+  return false;
+}
+
+export default function initMenu() {
+  menu.onClick = toggleMenu;
+  menu.onOuterClick = hideMenu;
+}
