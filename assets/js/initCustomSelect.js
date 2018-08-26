994daa375c8ea5f33a21ec01ae1cf6f9c6939764
Vitaly Krenel <vitaly.post@yahoo.com>
2018-08-26 15:01:05 +1100

    Make initCustomSelect responsible only for event handlers initialization

--- assets/js/initCustomSelect.js
+++ assets/js/initCustomSelect.js
@@ -1,84 +1,58 @@
-import Selectable from './selectable.js';
+import CustomSelect from './customSelect.js';
+import Popup from './popup.js';
+
+function selectClickHandler(select, popup) {
+  select.makeActive();
+  popup.show();
+}
+
+function popupClickHanlder(select, popup) {
+  select.makeInactive();
+  popup.hide();
+}
+
+function optionClickHandler(customSelect, popup, option) {
+  const activeOption = customSelect.options.find(
+    opt => opt.value === customSelect.select.value,
+  );
+
+  activeOption.makeInactive();
+  option.makeActive();
+
+  popup.hide();
+  customSelect.select.makeInactive();
+
+  return option;
+}
 
 export default function initCustomSelect() {
-  const ns = 'data-cs'; // Custom select namespace
+  const customSelectMatch = `[${CustomSelect.namespace}]`;
+  const customSelectNodeList = document.querySelectorAll(customSelectMatch);
 
-  const attrs = {
-    select: ns.concat('-select'),
-    option: ns.concat('-option'),
-    default: ns.concat('-default'),
-    value: ns.concat('-value'), 
-    popup: ns.concat('-popup'),
-  };
-
-  const selectors = {
-    namespace: `[${ns}]`,
-    select: `[${attrs.select}]`,
-    option: `[${attrs.option}]`,
-    default: `[${attrs.default}]`,
-    popup: `[${attrs.popup}]`,
-  };
-
-  // Active state CSS classes for active (or default) option and select
-  const activeState = {
-    select: 'custom-select__select--active',
-    option: 'custom-select__option--active',
-    popup: 'custom-select__options-backdrop--visible',
-  };
-
-  const customSelectNodeList = document.querySelectorAll(selectors.namespace);
-
-  customSelectNodeList.forEach((customSelectNode) => {
-    const selectNode = customSelectNode.querySelector(selectors.select);
-    const select = new Selectable(selectNode, activeState.select, attrs.value);
-
-    const optionNodeList = customSelectNode.querySelectorAll(selectors.option);
-    const options = Array.from(optionNodeList).map(
-      optionNode => new Selectable(optionNode, activeState.option, attrs.value),
-    );
-
-    // TODO: Add check: Missing default option
-    const defaultOptionNode = customSelectNode.querySelector(selectors.default);
-    const defaultOption = new Selectable(defaultOptionNode, activeState.option, attrs.value);
-
-    const popupNode = customSelectNode.querySelector(selectors.popup);
-
-    // Hack: popup block does not require settable properties (value and text),
-    // but for the sake of code simplicity I've decided to not use the class
-    // composition trick (.. extends C(B()))
-    const popup = new Selectable(popupNode, activeState.popup);
-
-    select.value = defaultOption.value;
-    select.text = defaultOption.text;
-
-    defaultOption.makeActive();
+  customSelectNodeList.forEach((node) => {
+    const customSelect = new CustomSelect(node);
+    const popupNode = node.querySelector('[data-popup]');
+    const popup = new Popup(popupNode, {
+      containerClass: 'custom-select__options-container--visible',
+      popupClass: 'custom-select__options--visible',
+    });
 
     // Click on select button will trigger popup
-    select.onClick(() => {
-      select.makeActive();
-      popup.makeActive();
+    customSelect.select.onClick(() => {
+      selectClickHandler(customSelect.select, popup);
     });
 
     // Click on popup backdrop will deactivate select
     popup.onClick(() => {
-      popup.makeInactive();
-      select.makeInactive();
+      popupClickHanlder(customSelect.select, popup);
     });
 
-    options.forEach((option) => {
+    customSelect.options.forEach((option) => {
       option.onClick(() => {
-        const activeOption = options.find(
-          opt => opt.value === select.value,
-        );
+        const selectedOption = optionClickHandler(customSelect, popup, option);
 
-        activeOption.makeInactive();
-        option.makeActive();
-
-        select.value = option.value;
-        select.text = option.text;
-
-        popup.makeInactive();
-        select.makeInactive();
+        customSelect.select.value = selectedOption.value;
+        customSelect.select.text = selectedOption.text;
       });
     });
   });
