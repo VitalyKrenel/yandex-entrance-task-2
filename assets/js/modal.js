8d2a0b026c6df26d31c2d1ecbff541007686edca
Vitaly Krenel <vitaly.post@yahoo.com>
2018-08-28 03:00:29 +1100

    Allow to set onConfirm and onClick handlers

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -10,29 +10,43 @@ export default class Modal extends Popup {
     this.modal = this.container.querySelector(Modal.selectors.body);
     this.closeButton = this.container.querySelector(Modal.selectors.close);
     this.confirmButton = this.container.querySelector(Modal.selectors.confirm);
+  }
 
-    const close = () => {
-      this.hide();
-    };
+  set onClose(handler) {
+    this.onCloseHandler = handler;
 
     if (this.closeButton) {
-      this.closeButton.addEventListener('click', close);
+      this.closeButton.addEventListener('click', this.onCloseHandler);
     }
+  }
+
+  get onClose() {
+    return this.onCloseHandler;
+  }
+
+  set onConfirm(handler) {
+    this.onConfirmHandler = handler;
 
     if (this.confirmButton) {
-      this.confirmButton.addEventListener('click', close);
+      this.confirmButton.addEventListener('click', this.onConfirmHandler);
     }
+  }
+
+  get onConfirm() {
+    return this.onConfirmHandler;
   }
 
   static get attributes() {
     const namespace = 'data-modal';
     const body = namespace.concat('-body');
     const close = namespace.concat('-close');
+    const open = namespace.concat('-open');
     const confirm = namespace.concat('-confirm');
 
     return {
       namespace,
       body,
+      open,
       close,
       confirm,
     };
